c6cb479bce5872ddb919fa8c19e9fba78113a644
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-12 13:56:08 +0100

    Horizontal player movement controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    private CharacterController _controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //get horizontal input
+        float horizontalInput = Input.GetAxis("Horizontal");
+        //define direction based on input
+        Vector3 direction = new Vector3(horizontalInput, 0, 0);
+
+        //MOVE based on that direction
+        _controller.Move(direction * Time.deltaTime);
+    }
+}
