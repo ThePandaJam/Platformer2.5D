0de1ca2c078984d8745ecaff32c590f7c6562353
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-13 13:53:23 +0100

    added velocity and gravity

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
     private CharacterController _controller;
+
+    [SerializeField]
+    private float _speed = 5.0f;
+    [SerializeField]
+    private float _gravity = 1.0f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -15,12 +20,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //get horizontal input
         float horizontalInput = Input.GetAxis("Horizontal");
-        //define direction based on input
         Vector3 direction = new Vector3(horizontalInput, 0, 0);
+        Vector3 velocity = direction * _speed;
 
-        //MOVE based on that direction
-        _controller.Move(direction * Time.deltaTime);
+        //if grounded
+        if (_controller.isGrounded)
+        {
+            //do nothing, jump later
+        }
+        else
+        {
+            velocity.y -= _gravity;
+        }
+        _controller.Move(velocity * Time.deltaTime);
     }
 }
