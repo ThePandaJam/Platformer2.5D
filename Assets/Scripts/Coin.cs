b457df197d322efa84466f3ae4d3f0f8b2bbb4c9
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-13 15:29:10 +0100

    Created coin collection (UI + logic)

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour
+{
+    //onTriggerEnter
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Player player = other.GetComponent<Player>();
+            if(player != null)
+            {
+                player.CoinIncrement();
+            }
+            Destroy(this.gameObject);
+        }
+    }
+    //increment coin count in player
+    //notify UI via player
+    //destroy coin object
+}
---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    private Text _coinCountText;
+    //update coin display()
+    void Start()
+    {
+        _coinCountText.text = "Coins: " + 0;
+    }
+    public void UpdateCoinDisplay(int coins)
+    {
+        _coinCountText.text = "Coins: " + coins;
+    }
+}
