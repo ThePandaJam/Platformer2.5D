fb30ce4f8f78e28163a02cc71a1f3da2289ee8d3
ThePandaJam <veronika.kravchenko@barbal.co>
2022-04-14 10:16:14 +0100

    created moving platform and script

---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    [SerializeField]
+    private Transform _targetA, _targetB;
+    private float _speed = 2.0f;
+    private bool _returnToBase = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_returnToBase == false)
+        {
+            //move to target b
+            transform.position = Vector3.MoveTowards(transform.position, _targetB.position, _speed * Time.deltaTime);
+        }
+        else if (_returnToBase)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetA.position, _speed * Time.deltaTime);
+        }
+        //go to point b
+        //if at point b
+        if (transform.position == _targetB.position)
+        {
+            _returnToBase = true;
+        }
+        //  go to point a
+        //if at point a
+        if(transform.position == _targetA.position)
+        {
+            _returnToBase = false;
+        }
+        //  go to point b
+    }
+}
